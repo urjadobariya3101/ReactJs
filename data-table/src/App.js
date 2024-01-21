6b2172e5d73de46f1c9637526e0ed25631e5fa56
Urja <uradobariya3101@gmail.com>
2024-01-21 17:08:14 +0530

    filtering done

--- data-table/src/App.js
+++ data-table/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data";
 
 function App() {
+  const [search, setSearch] = useState("");
+  // console.log(search);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control bg-dark text-white"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search Contact"
+        />
+      </div>
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .filter((val) => {
+              return search.toLowerCase() === ''
+                ? val
+                : val.first_name.toLowerCase().includes(search);
+            })
+            .map((val) => {
+              return (
+                <tr key={val.id}>
+                  <td>{val.first_name}</td>
+                  <td>{val.last_name}</td>
+                  <td>{val.email}</td>
+                  <td>{val.phone}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
   );
 }
 
